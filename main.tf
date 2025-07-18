6b88452a06f82b3e6d5159893e46a31b7fc17d78
KOAMaximilianoAlarcon <103137982+KOAMaximilianoAlarcon@users.noreply.github.com>
2025-07-18 16:40:51 -0300

    Add bucket

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
+/*
 resource "aws_instance" "mi_ec2" {
   ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2023 en us-east-1
   instance_type = "t2.micro"
@@ -10,6 +11,7 @@ resource "aws_instance" "mi_ec2" {
     Name = "MiInstanciaTerraform"
   }
 }
+*/
 
 module "backend" {
   source = "./backend"
